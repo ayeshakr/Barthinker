365fc0470ce3e9c541615f533c6fff3130457756
ayeshakr <ayesha.kri@hotmail.com>
2014-09-20 14:48:57 -0400

    moar comments

--- Barthinker.py
+++ Barthinker.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Give the user a list of ingredients for a mixed drink and give them 4 names of drinks to choose from
 
 =======
@@ -7,10 +6,10 @@
 #Open file1, which will contain the lists of ingredients.
 #Randomly select one from file1.
 #Open file2 which contains the list of drinks.
-#generate a list of answers including the corresponding answer.
+#generate a list of answers including the corresponding answer. This list will add the drinks in a random order BUT must include the correct answer.
 #Print the list of answers to the screen and ask which drink the ingredients
-#would be used to make.
+#would be used to make. The user can input the answer by typing in the name of the drink?
 #Do this 5 times making sure that after a line has been randomly selected
 #from the file1, that it is not selected again and also that the answer to the
 #corresponding list of ingredients is placed in a random order.
->>>>>>> origin/master
+
