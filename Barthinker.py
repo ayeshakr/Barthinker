7c658c76f1a171f6d77978f32d7b5a611ca41bad
Stefan Davies <stefandavies@gmail.com>
2014-09-20 21:26:08 +0100

    New code

--- Barthinker.py
+++ Barthinker.py
@@ -1,3 +1,4 @@
+import random
 #Give the user a list of ingredients for a mixed drink and give them 4 names
 #of drinks to choose from
 #Open file1, which will contain the lists of ingredients.
@@ -9,3 +10,42 @@
 #Do this 5 times making sure that after a line has been randomly selected
 #from the file1, that it is not selected again and also that the answer to the
 #corresponding list of ingredients is placed in a random order.
+
+
+#open ingredients
+ingr = open('ingredients.txt')
+rez = []
+
+#open names
+names = open('names.txt')
+rez2 = []
+
+#add all the names to a list called rez2
+for lines in names:
+    rez2.append(lines)
+    
+    
+#add every line from ingredients to a list called ingr and
+#create a variable called index which holds a number between 0 and 8(inclusive).
+#print the ingredients at this index of the list.
+#the name corresponding to these ingredients are held int the variable answer
+#Now create a list of size 4 which includes the answer and 3 wrong answers.
+#Then print the ingredients and the 4 possible answers.
+for line in ingr:
+    rez.append(line)
+    index = randrange(8)
+    print(rez[index])
+    answer = rez2[index]
+    y = []
+    
+    
+        
+
+        
+
+        
+
+
+
+        
+        
